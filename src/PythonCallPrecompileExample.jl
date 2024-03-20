57c8104a1388772fbcf7a26acc902e0f38dfee13
Gustavo Nunes Goretkin <gustavo.goretkin@gmail.com>
2024-03-20 10:27:27 -0400

    Julia 1.10 causing some issues

--- src/PythonCallPrecompileExample.jl
+++ src/PythonCallPrecompileExample.jl
@@ -1,7 +1,7 @@
 module PythonCallPrecompileExample
 
 using PrecompileTools
-
+using Serialization: serialize, deserialize
 
 function foo(xs)
     [i*x for (i, x) in enumerate(xs)]
@@ -9,30 +9,31 @@ end
 
 # ENV["JULIA_CONDAPKG_BACKEND"] = "Null" # use the system installation of Python
 using PythonCall: pyexec, pyexec
-# pyexec("import sys; print(sys.executable)", Main)
 
-function get_precompilation()
+py_precompile_script = """
+import numpy as np
+# Use this way to make an anonymous module to avoid:
+#         Info Given PythonCallPrecompileExample was explicitly requested, output will be shown live
+# ERROR: LoadError: Python: Julia: Creating a new global in closed module `Main` (`PythonCallPrecompileExample`) breaks incremental compilation because the side effects will not be permanent.
+
+jl = juliacall.Base.Module()
+
+jl.seval("using PythonCallPrecompileExample")
+
+xs = np.random.rand(2, 2)
+ans = jl.PythonCallPrecompileExample.foo(xs)
+"""
+
+function get_precompilation_list()
     pyexec("import sys; print(sys.executable)", Main)
-
-    py_script = """
-    import numpy as np
-    from juliacall import Main as jl
-
-    jl.seval("using PythonCallPrecompileExample")
-
-    xs = np.random.rand(2, 2)
-    ans = jl.PythonCallPrecompileExample.foo(xs)
-    """
 
     Core.Compiler.Timings.reset_timings()
     Core.Compiler.__set_measure_typeinf(true)
 
-    pyexec(py_script, Main)
+    pyexec(py_precompile_script, Main)
 
     Core.Compiler.__set_measure_typeinf(false)
     Core.Compiler.Timings.close_current_timer()
-
-
 
     precompile_method_instances = Any[]
     function precompile_mi(mi)
@@ -73,32 +74,58 @@ function get_precompilation()
     return precompile_method_instances
 end
 
+function is_serializable(x)
+    try
+        open("/tmp/garbage.jls", "w") do file
+            serialize(file, x)
+        end
+        return true
+    catch e
+        if e isa MethodError
+            return false
+        end
+        rethrow(e)
+    end
+end
+
+# PythonCallPrecompileExample.write_precompilation_list(filter(PythonCallPrecompileExample.is_serializable, pcl))
+function write_precompilation_list(precompilation_list)
+    open("/tmp/precompilation_list.jls", "w") do file
+        serialize(file, precompilation_list)
+    end
+end
+
+
+
+# Uncomment this to hook into the precompilation. But it's circular, and it is giving
+
 module PlaceholderForPrecompilation
 end
 
-# Uncomment this to hook into the precompilation. But it's circular, and it is giving
-# Precompiling PythonCallPrecompileExample
-#         Info Given PythonCallPrecompileExample was explicitly requested, output will be shown live
-# ERROR: LoadError: Python: Julia: Creating a new global in closed module `Main` (`PythonCallPrecompileExample`) breaks incremental compilation because the side effects will not be permanent.
+# This is the standard precompilation approach, but on Julia v1.10.2 and PythonCall v0.9.15 it causes
+# ERROR: LoadError: Python: Julia: Evaluation into the closed module `anonymous` breaks incremental compilation because the side effects will not be permanent. This is likely due to some other module mutating `anonymous` with `eval` during precompilation - don't do this.
+# module PlaceholderForPrecompilation
+# end
 
-# @setup_workload begin
-#     ENV["JULIA_CONDAPKG_BACKEND"] = "Null" # use the system installation of Python
-#     using PythonCall: pyexec
-#     py_script = """
-#     import numpy as np
-#     from juliacall import Main as jl
+@setup_workload begin
+    ENV["JULIA_CONDAPKG_BACKEND"] = "Null" # use the system installation of Python
+    using PythonCall: pyexec
 
-#     jl.seval("using PythonCallPrecompileExample")
+    @compile_workload begin
+        # all calls in this block will be precompiled, regardless of whether
+        # they belong to this package or not (on Julia 1.8 and higher)
+        pyexec(py_precompile_script, PlaceholderForPrecompilation)
+    end
+end
 
-#     xs = np.random.rand(2, 2)
-#     ans = jl.PythonCallPrecompileExample.foo(xs)
-#     """
 
-#     @compile_workload begin
-#         # all calls in this block will be precompiled, regardless of whether
-#         # they belong to this package or not (on Julia 1.8 and higher)
-#         pyexec(py_script, PlaceholderForPrecompilation)
+# try
+#     pcl = deserialize("/tmp/precompilation_list.jls")
+#     for mi in pcl
+#         precompile(mi.specTypes)
 #     end
+# catch e
+#     @show e
 # end
 
 end # module PythonCallPrecompileExample
